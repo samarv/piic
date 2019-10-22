e045042df659bab7b9d28dba2eb129640ccf9f30
samarvir <samarvir1996@gmail.com>
2019-10-22 14:58:12 +0530

    Add classToken

--- calc1.cpp
+++ calc1.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 
 const std::string INTEGER = "INTEGER";
 const std::string PLUS = "PLUS";
 const std::string EoF = "EoF";
+
+using namespace std;
+class Token
+{
+public:
+    string type;
+    string value;
+    Token(string type, string value)
+    {
+        type = type;
+        value = value;
+    }
+};
 
 int main(void)
 {
