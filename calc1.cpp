a9a92c4d212dd1c4c9ba9060bdc694960506ff39
samarvir <samarvir1996@gmail.com>
2019-10-22 15:19:10 +0530

    add Str

--- calc1.cpp
+++ calc1.cpp
@@ -11,14 +11,21 @@ class Token
 public:
     string type;
     string value;
-    Token(string type, string value)
+    Token(string type1, string value1)
     {
-        type = type;
-        value = value;
+        type = type1;
+        value = value1;
+    }
+    void str()
+    {
+        cout << "tokenType => " << type << " tokenVal => " << value;
     }
 };
 
 int main(void)
 {
     std::cout << "initialize -> " << INTEGER << std::endl;
+    Token token = Token("Hello", "Bye");
+    std::cout << "VAlue => " << token.value << std::endl;
+    token.str();
 }
